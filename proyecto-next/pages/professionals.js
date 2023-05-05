6c36efd1ed3db872a9a3a4ba1ee27ba96cffe7d6
FabianIMV <fabi.munozv@duocuc.cl>
2023-05-05 18:02:10 -0400

    professionals filter
    aqui se debe agregar el filtro para buscar profesionales, sujeto a cambio de naaambre

---
+++ proyecto-next/pages/professionals.js
@@ -0,0 +1,12 @@
+import Professionals from '@/components/Professionals'
+import React from 'react'
+
+const professionals = () => {
+  return (
+    <div className='section-professionals'>
+      <Professionals />
+    </div>
+  )
+}
+
+export default professionals

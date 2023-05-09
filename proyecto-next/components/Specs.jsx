e9b32163ff5803a09bd6ae3a7c1aa77e5fb1930c
Francisco Salazar <fatzuack@gmail.com>
2023-05-08 21:42:24 -0400

    modificacion final

--- proyecto-next/components/Specs.jsx
+++ proyecto-next/components/Specs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Cards } from './index'
+import Image from 'next/image'
+import ImageInput from '../public/imaginput.jpeg'
 
 const Specs = () => {
   return (
   <div className='container-specs'>
     <div className='input-specs'>
+      <Image src={ImageInput} alt='poto caca peo' width={800} height={450}/>
       <input type="text" placeholder='Especifica tu problema' />
 
      </div>

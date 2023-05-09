d613255032e0ffa36c917b288e0cb78d0a0d122a
Francisco Salazar <fatzuack@gmail.com>
2023-05-08 21:26:49 -0400

    modificacion

--- proyecto-next/components/Specs.jsx
+++ proyecto-next/components/Specs.jsx
@@ -4,11 +4,7 @@ import { Cards } from './index'
 const Specs = () => {
   return (
   <div className='container-specs'>
-<<<<<<< HEAD
     <div className='input-specs'>
-=======
-    <div className='input-spec'>
->>>>>>> b98834982cdd789ae84c919c4bd923156e222feb
       <input type="text" placeholder='Especifica tu problema' />
 
      </div>

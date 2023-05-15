a7109194626225714cc9d4accf172fed29131ef0
Liqui0-Blvck <nicolasmacgwrk@gmail.com>
2023-05-15 15:59:12 -0400

    CAMBIO EN PROFESIONALS

--- proyecto-next/components/Professionals/Professionals.jsx
+++ proyecto-next/components/Professionals/Professionals.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { userData } from '../../utils/userData'
 import Cards from '../Cards'
 import TextField from '@mui/material/TextField';
+import Pagination from '@mui/material/Pagination';
 
 
 function Professionals() {
   const [profesionales, setProfesionales] = useState([])
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
 
   useEffect(() => {
     async function getData(){
@@ -28,6 +31,7 @@ function Professionals() {
   const handleFilterChange = (event) => {
     const { name, value } = event.target;
     setFilters({ ...filters, [name]: value });
+    setPage(1);
   };
 
   const handleResetFilters = () => {
@@ -38,19 +42,31 @@ function Professionals() {
       comuna: "",
       cuidad: "",
     });
+    setPage(1);
   };
 
-  const filteredProfessionals = profesionales.filter((profesionales) => {
+  const filteredProfessionals = profesionales.filter((profesional) => {
     return (
-      (filters.nombre === "" || profesionales.nombre.toLowerCase().startsWith(filters.nombre.toLowerCase())) &&
-      (filters.profesion === "" || profesionales.profesion.toLowerCase().startsWith(filters.profesion.toLowerCase())) &&
-      (filters.region === "" || profesionales.region.toLowerCase().startsWith(filters.region.toLowerCase())) &&
-      (filters.comuna === "" || profesionales.comuna.toLowerCase().startsWith(filters.comuna.toLowerCase())) &&
-      (filters.cuidad === "" || profesionales.cuidad.toLowerCase().startsWith(filters.cuidad.toLowerCase()))
+      (filters.nombre === "" || profesional.nombre.toLowerCase().startsWith(filters.nombre.toLowerCase())) &&
+      (filters.profesion === "" || profesional.profesion.toLowerCase().startsWith(filters.profesion.toLowerCase())) &&
+      (filters.region === "" || profesional.region.toLowerCase().startsWith(filters.region.toLowerCase())) &&
+      (filters.comuna === "" || profesional.comuna.toLowerCase().startsWith(filters.comuna.toLowerCase())) &&
+      (filters.cuidad === "" || profesional.cuidad.toLowerCase().startsWith(filters.cuidad.toLowerCase()))
     );
   });
 
-  
+  const pagedProfessionals = filteredProfessionals.slice((page - 1) * pageSize, page * pageSize);
+
+  const totalPages = Math.ceil(filteredProfessionals.length / pageSize);
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const handlePageSizeChange = (event) => {
+    setPageSize(event.target.value);
+    setPage(1);
+  };
 
   return (
     <div className="container-pro">
@@ -58,22 +74,34 @@ function Professionals() {
       
       <div className="low-section">
         <form className="form-pro">
-          <input type="text" name="profesion" value={filters.profesion} placeholder="Profesion" onChange={handleFilterChange} />
-          <input type="text" name="region" value={filters.region} placeholder="Region" onChange={handleFilterChange} />
-          <input type="text" name="comuna" value={filters.comuna} placeholder="Comuna" onChange={handleFilterChange} />
-          <input type="text" name="cuidad" value={filters.cuidad} placeholder="Cuidad" onChange={handleFilterChange} />
-        <button type="button" onClick={handleResetFilters}>
-          Reset Filters
-        </button>
-      </form>
-      <div className="box-profesional">
-        {filteredProfessionals.map((profesional) => (
-          <Cards key={profesional.id_profesional} id={profesional.id_profesional} profesion={profesional.profesion} name={profesional.nombre} email={profesional.correo_electronico} bio={profesional.biografia}/>
-        ))}
+          <TextField color='secondary' label="Profesion" name="profesion" value={filters.profesion} onChange={handleFilterChange} />
+          <TextField color='secondary' label="Region" name="region" value={filters.region} onChange={handleFilterChange} />
+          <TextField color='secondary' label="Comuna" name="comuna" value={filters.comuna} onChange={handleFilterChange} />
+          <TextField color='secondary' label="Ciudad" name="cuidad" value={filters.cuidad} onChange={handleFilterChange} />
+          <button type="button" onClick={handleResetFilters}>
+            Reset Filters
+          </button>
+        </form>
+        <div className="box-profesional">
+          <div className="box-filter_pro">
+            <TextField label="Limit" type="number" value={pageSize} onChange={handlePageSizeChange} />
+          </div>
+
+          <div className="box-cards">
+            {filteredProfessionals
+              .slice((page - 1) * pageSize, page * pageSize)
+              .map((profesional) => (
+                <Cards key={profesional.id_profesional} id={profesional.id_profesional} profesion={profesional.profesion} name={profesional.nombre} email={profesional.correo_electronico} bio={profesional.biografia}/>
+              ))}
+          </div>
+
+          <div className="pagination">
+            <Pagination count={totalPages} page={page} onChange={(event, value) => {setPage(value);}}/>
+          </div>
+        </div>
       </div>
     </div>
-</div>
-);
+  );
 }
 
 export default Professionals;
